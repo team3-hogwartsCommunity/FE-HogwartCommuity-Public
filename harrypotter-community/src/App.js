e631049cee7602a488833b00a49e758160cd0193
t3qquq <t3qquq@gmail.com>
2023-03-01 01:41:26 +0900

    #004 기숙사테스트 페이지 형태 구현

--- harrypotter-community/src/App.js
+++ harrypotter-community/src/App.js
@@ -1,12 +1,14 @@
-import React from 'react'
-import Join from './pages/Join'
-import Login from './pages/Login'
+import React from "react";
+import Join from "./pages/Join";
+import Login from "./pages/Login";
+import HouseSort from "./pages/HouseSort";
 
 function App() {
   return (
-    <Login />
+    // <Login />
     // <Join />
-  )
+    <HouseSort />
+  );
 }
 
-export default App
+export default App;
---
+++ harrypotter-community/src/HouseSortData.js
@@ -0,0 +1,47 @@
+export const questions = [
+  {
+    question: "예시질문 1번입니다.",
+    answers: {
+      answerGriff: "그리핀도르스러운 답변1",
+      answerHuff: "후플푸프스러운 답변1",
+      answerRaven: "레번클로스러운 답변1",
+      answerSlyth: "슬리데린스러운 답변1",
+    },
+  },
+  {
+    question: "예시질문 2번입니다.",
+    answers: {
+      answerGriff: "그리핀도르스러운 답변2",
+      answerHuff: "후플푸프스러운 답변2",
+      answerRaven: "레번클로스러운 답변2",
+      answerSlyth: "슬리데린스러운 답변2",
+    },
+  },
+  {
+    question: "예시질문 3번입니다.",
+    answers: {
+      answerGriff: "그리핀도르스러운 답변3",
+      answerHuff: "후플푸프스러운 답변3",
+      answerRaven: "레번클로스러운 답변3",
+      answerSlyth: "슬리데린스러운 답변3",
+    },
+  },
+  {
+    question: "예시질문 4번입니다.",
+    answers: {
+      answerGriff: "그리핀도르스러운 답변4",
+      answerHuff: "후플푸프스러운 답변4",
+      answerRaven: "레번클로스러운 답변4",
+      answerSlyth: "슬리데린스러운 답변4",
+    },
+  },
+  {
+    question: "예시질문 5번입니다.",
+    answers: {
+      answerGriff: "그리핀도르스러운 답변5",
+      answerHuff: "후플푸프스러운 답변5",
+      answerRaven: "레번클로스러운 답변5",
+      answerSlyth: "슬리데린스러운 답변5",
+    },
+  },
+];
