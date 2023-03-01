042f7e2dd4e049b11ea955084dc8c4d91d08f278
EUNBINs <dmsqls891@naver.com>
2023-03-02 00:57:57 +0900

    # update : 메인페이지 CSS 적용

--- harrypotter-community/src/components/FullBoard.js
+++ harrypotter-community/src/components/FullBoard.js
@@ -6,48 +6,46 @@ import './boardPaging.css'
 import Pagination from 'react-js-pagination';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
-
 import Header from './Header';
 
-const BoardContainer = styled.div`
-    display: flex;
-    flex-direction: row;
-    flex-wrap: wrap;
-    height: 1200px;
-  `
+// const BoardContainer = styled.div`
+//     display: flex;
+//     flex-direction: row;
+//     flex-wrap: wrap;
+//     height: 1200px;
+//   `
 
-const BoardItem = styled.div`
-  width: 700px;
-  height: 40%;
-  margin-top: 50px;
-  margin-left: 70px;
-  margin-right: 70px;
-  background-color: white;
-  border: 1px solid black;
- 
-
-`
+// const BoardItem = styled.div`
+//   width: 700px;
+//   height: 40%;
+//   margin-top: 50px;
+//   margin-left: 70px;
+//   margin-right: 70px;
+//   background-color: white;
+//   border: 1px solid black;
+// `
 
 // 테이블 구현
 
 
 function FullBoard() {
-          //1
+
   const [currentPage, setCurrentPage] = useState(1);
-  console.log("현재 페이지 :" , currentPage)
+  // const [title, onChangeTitle, resetTitle] = useInput();
+  // const [contents, onChangeContents, resetContents] = useInput();
+  // const [dormitory, onChangeDormitory, resetDormitory] = useInput();
   const queryClient = useQueryClient()
-  // useinfiniteQuery
   const { isLoading, isError, data } = useQuery(
     ['board', currentPage-1],
     () => getGryffindorBoard(currentPage-1),
     {keepPreviousData:true}
-  )
- 
-  console.log("data : ", data)
-  console.log("현재 페이지 :" , currentPage)
+    )
 
-  
- 
+  // const addMutation = useMutation(addBoard,{
+  //   onSuccess : () => {
+  //     queryClient.invalidateQueries('board')
+  //   }
+  // })
   const deleteMutation = useMutation(deleteBoard, {
     onSuccess : () =>{
       queryClient.invalidateQueries('board')
@@ -63,23 +61,25 @@ function FullBoard() {
   if (isError) {
     return <h1>Error...</h1>
   }
-  
-  // console.log(data)
-
-  // console.log(data)
+  console.log(data)
 
   
 
   // 1, 10 , 11, 20, 21, 30
-  
- 
-  
+  const boardData = data.data.boardLists.slice((currentPage-1) * 8, (currentPage * 8))
   const paginationHandler = (i) => {
     
     setCurrentPage(i)
   }
 
-  
+  // const addDormBoard = (e) => {
+  //   e.preventDefault();
+  //   addMutation.mutate({
+  //     title,
+  //     contents,
+  //     dormitory
+  //   })
+  // }
   const deleteDormBoard = (boardId) => {
     
     deleteMutation.mutate(boardId)
@@ -89,58 +89,81 @@ function FullBoard() {
 
   return (
     <>
-      <Header/>
+    <Container>
+      <Header />
       {/* <form onSubmit={addDormBoard}> 
         <input type="text" name='title' value={title} onChange={onChangeTitle}/>
         <input type="contents" name='contents' value={contents} onChange={onChangeContents}/>
         <input type="dormitory" name='dormitory' value={dormitory} onChange={onChangeDormitory}/>
         <button type='submit'>추가 테스트</button>
       </form> */}
-      <div>
-        <BoardContainer>
+      <Bg>
+      <Wrap>
         {
-          data.data.boardLists.map((item) => (
-            <BoardItem key={item.id}>
-              <h2>{item.title}</h2>
-              <p>{item.sub}</p>
-              <button>좋아요</button>
-              
-              <button onClick={() => {deleteDormBoard(item.id)}}>삭제</button>
-              <button><Link to={`/EditPost/${item.id}`}>수정</Link></button>
-              
-              <div>
-              <Link to={`/board/${item.id}`}>보기</Link>
+          boardData.map((item) => (
+              <CardContainer border='#e96363'>
+              <div key={item.id}>
+              <Font size='20px'>{item.title}</Font>
+              <Font>{item.contents}</Font>
               </div>
-            </BoardItem>
+              </CardContainer>
+            // {/* <div key={item.id}>
+            //   <Font size='20px'>{item.title}</Font>
+            //   <Font>{item.contents}</Font>
+            //   {/* <button>좋아요</button>
+            //   <button>수정</button>
+            //   <button onClick={() => {deleteDormBoard(item.id)}}>삭제</button>
+            //   <div>
+            //   <Link to={`/board/${item.id}`}>보기</Link>
+            //   </div> */}
+            // </div> */}
           ))
         }
-        </BoardContainer>
-        {/* 페이지네이션 재구현 */}
-        {/* <nav className='d-flex justify-content-center'>
-          <ul className='pagination'>
-            {
-              pages.map(
-                page => <li className='page-link'
-                onClick={() => paginationHandler(page)}
-                key={page}>{page}</li>
-              )
-            }
-          </ul>
-        </nav> */}
+        </Wrap>
         <Pagination
           activePage={currentPage}
           itemsCountPerPage={8}
-          totalItemsCount={data.data.totalPages}
+          totalItemsCount={data.data.length}
           pageRangeDisplayed={5}
           prevPageText={"<"}
           nextPageText={">"}
           onChange={paginationHandler}
         
         />
-      </div>
+      </Bg>
+    </Container>
     </>
-
   )
 }
+
+const Container = styled.div`
+  background-color: black;
+`
+const Bg = styled.div`
+  height: 100vh;
+  width: 100vw;
+  background-color: black;
+`
+const Wrap = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  margin: 30px 100px;
+  gap: 50px;
+`
+const CardContainer = styled.div`
+  width: 300px;
+  height: 250px;
+  border-radius: 8px;
+  border: 1px solid ${({ border }) => border};
+  box-shadow: 2px 1px 5px 2px ${({ border }) => border};;
+  padding: 20px;
+`
+
+const Font = styled.div`
+  color: white;
+  font-size: ${({ size }) => size};
+  font-family: 'lightFont';
+`
 
 export default FullBoard
